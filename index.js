d55d2ae87fc6452e5d41be371da6d71a9d144c2a
Geoffrey <you@example.comcohengmc@gmail.com>
2024-02-21 07:44:44 -0500

    took away inline function call. Yeah i'm a badass

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let items = []
-listEl = document.querySelector("#list")
-inputEl = document.querySelector("#text-input")
-saveBtnEl = document.querySelector("#save-btn")
+let listEl = document.querySelector("#list")
+let inputEl = document.querySelector("#text-input")
+let saveBtnEl = document.querySelector("#save-btn")
+let trashEls = document.querySelectorAll(".trash")
 
 inputEl.addEventListener("keypress", function (e) {
     if (e.key == "Enter") {
@@ -22,20 +23,30 @@ function saveItem() {
     render()
 }
 
-function remove(removeMe) {
+function remover(removeMe) {
     let clean = removeMe + ""
     clean = removeMe.substr(3, clean.length)
     items.splice(clean, 1)
-    console.log(removeMe);
     render()
+}
+
+function checkRemoval() {
+    trashEls = document.querySelectorAll(".item")
+    for(let i = 0; i < trashEls.length; i++) {
+        trashEls[i].addEventListener("click", function() {
+            remover(trashEls[i].getAttribute("id"));
+        })
+    }
 }
 
 function render() {
     listEl.innerHTML = ""
     for (let i = 0; i < items.length; i++) {
         listEl.innerHTML += `
-        <li id="btn${i}" onclick="remove(id)">${items[i]} <img class="trash" src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png" alt="trash can icon">
+        <li id="btn${i}" class="item">${items[i]} <img class="trash" src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png" alt="trash can icon">
         </li>
         `
     }
+
+    checkRemoval()
 }
