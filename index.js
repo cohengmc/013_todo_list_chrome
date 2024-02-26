eaf1b9c9068e65559da718d065f034b848f20f5e
Geoffrey <you@example.comcohengmc@gmail.com>
2024-02-26 06:34:25 -0500

    functioning chrome extension

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ let listEl = document.querySelector("#list")
 let inputEl = document.querySelector("#text-input")
 let saveBtnEl = document.querySelector("#save-btn")
 let trashEls = document.querySelectorAll(".trash")
+let deleteBtnEl = document.querySelector("#delete-all-btn")
+const itemsFromLocalStorage = JSON.parse(localStorage.getItem("toDoList"))
+
+if(itemsFromLocalStorage) {
+    items = itemsFromLocalStorage
+    render(items)
+}
 
 inputEl.addEventListener("keypress", function (e) {
     if (e.key == "Enter") {
@@ -20,18 +27,27 @@ function saveItem() {
     }
     items.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("toDoList", JSON.stringify(items) )
+    console.log("object");
     render()
 }
+
+deleteBtnEl.addEventListener("click", function () {
+    items = []
+    localStorage.clear()
+    render()
+})
 
 function remover(removeMe) {
     let clean = removeMe + ""
     clean = removeMe.substr(3, clean.length)
     items.splice(clean, 1)
+    localStorage.setItem("toDoList", JSON.stringify(items) )
     render()
 }
 
 function checkRemoval() {
-    trashEls = document.querySelectorAll(".item")
+    trashEls = document.querySelectorAll(".trash")
     for(let i = 0; i < trashEls.length; i++) {
         trashEls[i].addEventListener("click", function() {
             remover(trashEls[i].getAttribute("id"));
@@ -43,7 +59,7 @@ function render() {
     listEl.innerHTML = ""
     for (let i = 0; i < items.length; i++) {
         listEl.innerHTML += `
-        <li id="btn${i}" class="item">${items[i]} <img class="trash" src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png" alt="trash can icon">
+        <li class="item">${items[i]} <img id="btn${i}" class="trash" src="https://cdn-icons-png.flaticon.com/128/3096/3096673.png" alt="trash can icon">
         </li>
         `
     }
